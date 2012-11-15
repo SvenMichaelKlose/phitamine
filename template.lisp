79f2bbb4bc9fbafda7dd1a0df6c5587c236c82c1
pixel <pixel@f372215d-759a-4a89-8466-d632cf8119e5>
2012-11-15 10:05:59 +0000

    TEMPLATE-PARAM-VALUE: Fixed for NIL values.
    TEMPLATE-LIST now returns HTML instead of LHTML.
    
    
    git-svn-id: file:///home/pixel/desk/code/repositories/phitamine@23 f372215d-759a-4a89-8466-d632cf8119e5

--- template.lisp
+++ template.lisp
@@ -4,13 +4,13 @@
 (defvar *currently-processed-templates* nil)
 
 (defun template-param-value (name &key (if-exists? nil))
-  (| (awhen (assoc name *template-parameters* :test #'eq)
-       .!)
-     (unless if-exists?
-       (princ "<br>Current template parameters:<br>")
-       (print *template-parameters*)
-       (princ "<br>")
-       (error "template parameter ~A not found" (symbol-name name)))))
+  (!? (assoc name *template-parameters* :test #'eq)
+      .!
+      (unless if-exists?
+        (princ "<br>Current template parameters:<br>")
+        (print *template-parameters*)
+        (princ "<br>")
+        (error "template parameter ~A not found" (symbol-name name)))))
 
 (defun template-param (name &key (if-exists? nil))
   (force-list (template-param-value name :if-exists? if-exists?)))
@@ -20,12 +20,13 @@
   `(defun ,name (&optional (params nil))
      (& (member ',name *currently-processed-templates*)
         (error "template ~A called recursively" ',name))
+     (flush)
      (with-temporaries (*template-parameters* (append params *template-parameters*)
                         *currently-processed-templates* (cons ',name *currently-processed-templates*))
        (apply #'+ (filter #'lml2xml ,(list 'backquote (dot-expand (read-file-all path))))))))
 
 (defun template-list (template records)
   (let index 0
-    (filter [with-temporary *template-parameters* (cons (cons 'index (1+! index)) *template-parameters*)
-              (funcall template _)]
-            records)))
+    (apply #'+ (filter [with-temporary *template-parameters* (cons (cons 'index (1+! index)) *template-parameters*)
+                         (funcall template _)]
+                       records))))
