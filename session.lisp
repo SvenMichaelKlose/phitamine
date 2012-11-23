3f31682971e1f4347826e9eb2c5dcfb71be9cc25
pixel <pixel@f372215d-759a-4a89-8466-d632cf8119e5>
2012-11-23 15:20:23 +0000

    Cleanup. Fixes inlining.
    git-svn-id: file:///home/pixel/desk/code/repositories/phitamine@40 f372215d-759a-4a89-8466-d632cf8119e5

--- session.lisp
+++ session.lisp
@@ -1,12 +1,11 @@
-;;;;; Copyright (C) 2012 Sven Michael Klose <pixel@copei.de>
+;;;;; phitamine – Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
 
 (session_start)
 
 (defun session (x)
-  (%setq nil (%transpiler-native "return (isset ($_SESSION[$" x "->n])) ?" 
-                                         "$_SESSION[$" x "->n] : " 
-                                         "NULL;"))
-  nil)
+  (%transpiler-native "(isset ($_SESSION[$" x "->n])) ?" 
+                      "$_SESSION[$" x "->n] : " 
+                      "NULL;"))
 
 (defun (= session) (v x)
   (%setq nil (%transpiler-native "$_SESSION[$" x "->n] = $" v))
