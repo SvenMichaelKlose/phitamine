a10256b01aef0afa5d4b162df638fd216c45ad80
pixel <pixel@f372215d-759a-4a89-8466-d632cf8119e5>
2012-12-05 18:52:18 +0000

    New struct SQL-TABLE.
    git-svn-id: file:///home/pixel/desk/code/repositories/phitamine@46 f372215d-759a-4a89-8466-d632cf8119e5

--- compile-time-sql-info.lisp
+++ compile-time-sql-info.lisp
@@ -2,10 +2,16 @@
 
 (defvar *sql-table-definitions* name)
 
+(defstruct sql-table
+  name
+  fields)
+
 (defstruct sql-field
   name
   sql-type
   type)
 
 (defun add-sql-table-definition (name fields)
-  (acons! name [cons _. (make-sql-field :name _. :sql-type ._. :type .._.)] *sql-table-definitions*))
+  (acons! name (make-sql-table :name name
+                               :fields (filter [cons _. (make-sql-field :name _. :sql-type ._. :type .._.)] fields))
+          *sql-table-definitions*))
