482850edc6f371640f5bb9e5cde4789259a655e8
Sven Michael Klose <pixel@copei.de>
2014-09-22 14:48:16 +0100

    Generate an .htaccess file for Apache to get the required URL rewrites.

--- make-project.lisp
+++ make-project.lisp
@@ -2,7 +2,18 @@
 
 (load "phitamine/compile-time-sql-info.lisp")
 
-(defun make-phitamine-project (name files)
+(defun print-htaccess-rules (out &key script-path)
+  (format out "Options -indexes~%")
+  (format out "RewriteEngine on~%")
+  (format out "RewriteCond %{REQUEST_FILENAME} !-f~%")
+  (format out "RewriteCond %{REQUEST_FILENAME} !-d~%")
+  (format out "RewriteRule .? ~A/index.php$1 [L]~%" script-path))
+
+(defun make-phitamine-project (name &key files script-path)
+  (| (cons? files)
+     (error "FILES is missing."))
+  (| (string? script-path)
+     (error "SCRIPT-PATH is missing."))
   (unix-sh-mkdir "compiled")
   (make-project name
                 (+ (read-file "phitamine/files.lisp")
@@ -11,4 +22,6 @@
                    files)
                 :transpiler  *php-transpiler*
                 :obfuscate?  nil
-                :emitter     [put-file "compiled/index.php" _]))
+                :emitter     [put-file "compiled/index.php" _])
+  (with-output-file out "compiled/.htaccess"
+    (print-htaccess-rules out :script-path script-path)))
