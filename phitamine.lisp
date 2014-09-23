3e0316cc2f7f9a607dd5636495cbefa0fd57f2e3
Sven Michael Klose <pixel@copei.de>
2014-09-23 10:15:20 +0100

    Fixed test if *HOME-COMPONENTS* is set correctly.

--- phitamine.lisp
+++ phitamine.lisp
@@ -1,8 +1,7 @@
 ;;;;; phitamine – Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
 
 (defun phitamine ()
-  (| (cons? *home-components*)
+  (| (list? *home-components*)
      (error "*HOME-COMPONENTS* must be a list of components"))
   (= *current-language* (detect-language))
-  (db-connect)
   (call-url-actions))
