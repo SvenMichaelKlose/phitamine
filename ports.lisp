695e63d0fcac2a326a1ef3b26274cc2ccfdacef6
Sven Michael Klose <pixel@copei.de>
2014-09-24 00:56:04 +0100

    Fix use of AADJOIN!.

--- ports.lisp
+++ ports.lisp
@@ -1,10 +1,10 @@
-;;;;; phitamine – Copyright (c) 2012–2013 Sven Michael Klose <pixel@copei.de>
+;;;;; phitamine – Copyright (c) 2012–2014 Sven Michael Klose <pixel@copei.de>
 
 (defvar *group-ports*     nil)
 (defvar *requested-ports* nil)
 (defvar *ports*           nil)
 
-(defun request-port   (port fun)    (aadjoin! fun port *requested-ports* :test #'eq))
+(defun request-port   (port fun)    (aadjoin! port fun *requested-ports* :test #'eq))
 (defun (= group-port) (port group)  (acons! group port *group-ports*))
 (defun group-port     (group)       (assoc-value group *group-ports*))
 (defun generate-ports ()            (= *ports* (filter [. _. (funcall ._)] *requested-ports*)))
