4d7bec17acf4bfeba13da9002b9a22b8b6d50ed4
Sven Michael Klose <pixel@copei.de>
2014-09-26 16:03:33 +0100

    Check if :LIMIT and :OFFSET are used together.

--- php/sql/select.lisp
+++ php/sql/select.lisp
@@ -8,6 +8,9 @@
             (alist-assignments ! :padding " AND ")))))
 
 (defun sql-clause-select (&key table (fields nil) (where nil) (limit nil) (offset nil) (order-by nil) (direction nil))
+  (when-debug
+    (when (== 1 (length (remove-if #'not (list limit offset))))
+      (error ":LIMIT and :OFFSET have to be used together.")))
   (apply #'string-concat
          `("SELECT " ,@(!? fields
                            (comma-separated-list (symbol-names ! :downcase? t))
