f308389dd0ab2b0b46fdd5de48ab21df3ad8aa21
Sven Michael Klose <pixel@copei.de>
2016-11-19 10:31:17 +0100

    Minor cleanup.

--- ports.lisp
+++ ports.lisp
@@ -8,7 +8,9 @@
 (defun (= group-port) (port group)  (acons! group port *group-ports*))
 (defun group-port     (group)       (assoc-value group *group-ports*))
 (defun generate-ports ()            (= *ports* (@ [. _. (funcall ._)] *requested-ports*)))
-(defun port           (x)           (| (assoc-value x (| *ports* (generate-ports))) ""))
+(defun port           (x)           (| (assoc-value x (| *ports*
+                                                         (generate-ports)))
+                                       ""))
 (defun action-group   (x)           ..x)
 (defun component-port (x)           (group-port (action-group (component-action x))))
 (defun components-w/o-port (port)   (remove-if [eq port (component-port _.)] *components*))
