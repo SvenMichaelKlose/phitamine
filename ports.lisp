26cae5b0b08b758b9d0809af17a548103902be03
Sven Michael Klose <pixel@copei.de>
2019-01-27 18:42:05 +0100

    Proper formatting.

--- ports.lisp
+++ ports.lisp
@@ -2,16 +2,31 @@
 (var *requested-ports* nil)
 (var *ports*           nil)
 
-(fn request-port   (port fun)    (aadjoin! port fun *requested-ports* :test #'eq))
-(fn (= group-port) (port group)  (acons! group port *group-ports*))
-(fn group-port     (group)       (assoc-value group *group-ports*))
-(fn generate-ports ()            (= *ports* (@ [. _. (funcall ._)] *requested-ports*)))
-(fn port           (x)           (| (assoc-value x (| *ports*
-                                                         (generate-ports)))
-                                       ""))
-(fn action-group   (x)           ..x)
-(fn component-port (x)           (group-port (action-group (component-action x))))
-(fn components-w/o-port (port)   (remove-if [eq port (component-port _.)] *components*))
+(fn request-port (port fun)
+  (aadjoin! port fun *requested-ports* :test #'eq))
+
+(fn (= group-port) (port group)
+  (acons! group port *group-ports*))
+
+(fn group-port (group)
+  (assoc-value group *group-ports*))
+
+(fn generate-ports ()
+  (= *ports* (@ [. _. (funcall ._)] *requested-ports*)))
+
+(fn port (x)
+  (| (assoc-value x (| *ports*
+                       (generate-ports)))
+     ""))
+
+(fn action-group (x)
+  ..x)
+
+(fn component-port (x)
+  (group-port (action-group (component-action x))))
+
+(fn components-w/o-port (port)
+  (remove-if [eq port (component-port _.)] *components*))
 
 (defmacro set-port (&rest body)
   `(request-port (group-port ..*action*) #'(() ,@body)))
