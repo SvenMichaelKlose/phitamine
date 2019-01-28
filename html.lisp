16013be44b5acc230c5925074e8affc9031737a6
Sven Michael Klose <pixel@copei.de>
2019-01-28 06:31:36 +0100

    HTML-OPTION: Use EQL instead of STRING== to find selected options.

--- html.lisp
+++ html.lisp
@@ -1,8 +1,8 @@
 (fn html-option (value name selected)
   (string-concat "<option value=\"" value "\""
-                (& (string== selected value)
-                   " selected")
-                ">" name "</option>"))
+                 (& (eql selected value)
+                    " selected")
+                 ">" name "</option>"))
 
 (fn html-options (x selected)
   (apply #'string-concat (@ [html-option _. ._ selected] x)))
