2df36f3be2450ea3c54b14f32873c70bd8b61f64
Sven Michael Klose <pixel@copei.de>
2019-01-28 07:22:59 +0100

    Improved readability.

--- define-sql-table.lisp
+++ define-sql-table.lisp
@@ -2,17 +2,18 @@
   (add-sql-table-definition name fields)
   (let table-name (downcase (symbol-name name))
     `(progn
-       (fn ,($ 'find- name) (&optional (fields nil) &key (limit nil) (offset nil) (order-by nil) (direction nil))
+       (fn ,($ 'find- name) (&optional (fields nil) ; TODO: Not used.
+                             &key (limit nil) (offset nil) (order-by nil) (direction nil))
          (let column-names (make-upcase-symbols (*db*.column-names (+ *db-table-prefix* ,table-name)))
-           (@ [@ #'cons column-names _]
-              (*db*.exec (sql-clause-select
-                             (make-selection-info :table      (+ *db-table-prefix* ,table-name)
-                                                  :where      (ensure-alist fields)
-                                                  :limit      limit
-                                                  :offset     offset
-                                                  :order-by   order-by
-                                                  :direction  direction))))))
-       (fn ,($ 'find- singular-name) (&optional (fields nil))
+           (!= (make-selection-info :table      (+ *db-table-prefix* ,table-name)
+                                    :where      (ensure-alist fields)
+                                    :limit      limit
+                                    :offset     offset
+                                    :order-by   order-by
+                                    :direction  direction)
+             (@ [@ #'cons column-names _]
+                (*db*.exec (sql-clause-select !))))))
+      (fn ,($ 'find- singular-name) (&optional (fields nil))
          (car (funcall #',($ 'find- name) fields)))
        (fn ,($ 'insert- singular-name) (fields)
          (*db*.exec (sql-clause-insert :table   (+ *db-table-prefix* ,table-name)
@@ -28,23 +29,24 @@
          (*db*.exec (sql-clause-delete :table  (+ *db-table-prefix* ,table-name)
                                        :where  (ensure-alist fields))))
        (fn ,($ 'get- singular-name '-count) (&optional (fields nil))
-         (number (caar (*db*.exec (sql-clause-select
-                                      (make-selection-info :table   (+ *db-table-prefix* ,table-name)
-                                                           :fields  '("COUNT(1)")
-                                                           :where   (ensure-alist fields)))))))
+         (!= (make-selection-info :table   (+ *db-table-prefix* ,table-name)
+                                  :fields  '("COUNT(1)")
+                                  :where   (ensure-alist fields))
+           (number (caar (*db*.exec (sql-clause-select !))))))
        (fn ,($ 'get-distinct- name) (field &key (where nil) (order-by nil) (direction nil))
-         (carlist (*db*.exec (sql-clause-select
-                                 (make-selection-info :table      (+ *db-table-prefix* ,table-name)
-                                                      :fields     `(,,(+ "DISTINCT(" (symbol-name field) ")"))
-                                                      :where      (ensure-alist where)
-                                                      :order-by   order-by
-                                                      :direction  direction)))))
-       (fn ,($ 'select- name) (&key (fields nil) (where nil) (limit nil) (offset nil) (order-by nil) (direction nil))
-         (*db*.exec (sql-clause-select
-                        (make-selection-inf :table      (+ *db-table-prefix* ,table-name)
-                                            :fields     fields
-                                            :where      (ensure-alist where)
-                                            :limit      limit
-                                            :offset     offset
-                                            :order-by   order-by
-                                            :direction  direction)))))))
+         (!= (make-selection-info :table      (+ *db-table-prefix* ,table-name)
+                                  :fields     `(,,(+ "DISTINCT(" (symbol-name field) ")"))
+                                  :where      (ensure-alist where)
+                                  :order-by   order-by
+                                  :direction  direction)
+           (carlist (*db*.exec (sql-clause-select !)))))
+       (fn ,($ 'select- name) (&key (fields nil) (where nil) (limit nil) (offset nil)
+                                    (order-by nil) (direction nil))
+         (!= (make-selection-inf :table      (+ *db-table-prefix* ,table-name)
+                                 :fields     fields
+                                 :where      (ensure-alist where)
+                                 :limit      limit
+                                 :offset     offset
+                                 :order-by   order-by
+                                 :direction  direction)
+           (*db*.exec (sql-clause-select !)))))))
